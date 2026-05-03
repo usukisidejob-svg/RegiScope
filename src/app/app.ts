ccd713326fe7c9e3bff248dd6ce344cbbd162382
rintarou_usuki <>
2026-05-03 16:35:36 +0900

    feat: Angular初期表示の削除、componentとの結び付け

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { LayoutComponent } from './shared/components/layout/layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [LayoutComponent],
+  template: `<app-layout />`
 })
-export class App {
-  protected readonly title = signal('RegiScope');
-}
+export class App {}
